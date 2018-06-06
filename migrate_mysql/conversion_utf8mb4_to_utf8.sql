6b0fb8d9c988945f18767b0f5520a504535f5fb1
alagoroVigo <alberto@alagoro.com>
2018-06-06 21:33:58 +0200

    volvemos al inicio

---
+++ migrate_mysql/conversion_utf8mb4_to_utf8.sql
@@ -0,0 +1,13 @@
+/* 
+ * @Copyright 2018, Alagoro Software. 
+ * @licencia   GNU General Public License version 2 or later; see LICENSE.txt
+ * @Autor Alberto Lago Rodríguez. Alagoro. alberto arroba alagoro punto com
+ * @Descripción	
+ */
+/**
+ * Author:  alagoro
+ * Created: 04-jun-2018
+ */
+
+ALTER TABLE proveedores CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE clientes CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
